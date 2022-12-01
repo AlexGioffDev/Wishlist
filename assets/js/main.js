676356fcc8360520d7b8491807afcba6ad54c915
Alex <alex.gioffre_dev@outlook.it>
2022-12-01 18:16:08 +0100

    Create the Jumbotron, Edit the structure of SCSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
-console.log("Funciona!!");
+let buttonCreate = document.getElementById("button-create");
+
+let storage = localStorage.getItem("wishlist");
+
+console.log(storage);
+
+if(storage == null) {
+    buttonCreate.innerText = "Create your Wishlist";
+} else {
+    buttonCreate.innerText = "See your Wishlist";
+}
+
